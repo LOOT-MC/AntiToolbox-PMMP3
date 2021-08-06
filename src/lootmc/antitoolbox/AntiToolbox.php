8b58b8830b3602aeae520561b4e18b3d6b41ba6b
AntonPPDS <60366294+AntonPPDS@users.noreply.github.com>
2021-08-06 17:30:03 +0300

    Update (v1.1) Fix bad command execution API to check if player using Toolbox

--- src/lootmc/antitoolbox/AntiToolbox.php
+++ src/lootmc/antitoolbox/AntiToolbox.php
@@ -9,17 +9,31 @@ use pocketmine\command\ConsoleCommandSender;
 
 use pocketmine\Player;
 
+use pocketmine\event\Listener;
+use pocketmine\event\server\DataPacketReceiveEvent;
+use pocketmine\event\player\PlayerLoginEvent;
+use pocketmine\event\player\PlayerQuitEvent;
 
-class AntiToolbox extends PluginBase {
+use pocketmine\network\mcpe\protocol\LoginPacket;
+
+use pocketmine\scheduler\ClosureTask;
+
+use lootmc\antitoolbox\event\ToolboxDetectedEvent;
+
+
+class AntiToolbox extends PluginBase implements Listener {
 
 	/** @var string[] */
 	private $onToolboxCmds;
+
+	/** @var array */
+	private $detected;
 
 	public function onEnable() {
 		$config = $this->getConfig();
 		$this->onToolboxCmds = $config->get("on-toolbox-cmds");
 
-		$this->getServer()->getPluginManager()->registerEvents(new ToolboxListener($this), $this);
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 	}
 
 	public function applyDefaultAction(Player $player) {
@@ -29,12 +43,42 @@ class AntiToolbox extends PluginBase {
 		}
 	}
 
-	public static function testToolbox(array $clientData) : bool {
-		if ($clientData["DeviceOS"] === 1) { // is Android
-			$modelSplit = explode(" ", $clientData["DeviceModel"]);
-			return $modelSplit[0] !== strtoupper($modelSplit[0]);
+	public function testToolbox(Player $player) {
+		return isset($this->detected[spl_object_hash($player)]);
+	}
+
+	public function handlePacketReceived(DataPacketReceiveEvent $ev) {
+		if ($ev->getPacket() instanceof LoginPacket) {
+			$clientData = $ev->getPacket()->clientData;
+			if ($clientData["DeviceOS"] === 1) { // is Android
+				$model = explode(" ", $clientData["DeviceModel"], 1)[0];
+				if ($model !== strtoupper($model)) {
+					$this->detected[spl_object_hash($ev->getPlayer())] = true;
+				}
+			}
 		}
-		return false;
+	}
+
+	/**
+	 * @priority LOW
+	 */
+	public function handleLogin(PlayerLoginEvent $ev) {
+		$player = $ev->getPlayer();
+		if ($this->testToolbox($player)) {
+			$this->getScheduler()->scheduleDelayedTask(new ClosureTask(function(int $tick) use ($player): void {
+				if ($player->isConnected()) {
+					$event = new ToolboxDetectedEvent($player);
+					$event->call();
+					if (!$event->isCancelled()) {
+						$this->applyDefaultAction($player);
+					}
+				}
+			}), 1);
+		}
+	}
+
+	public function handleQuit(PlayerQuitEvent $ev) {
+		unset($this->detected[spl_object_hash($ev->getPlayer())]);
 	}
 
 }
